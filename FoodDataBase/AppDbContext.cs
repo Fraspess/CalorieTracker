d1b293e6d26fbd88c5ae74be88b0d0ffebdace8c
Fraspess <pavlosamchyk@gmail.com>
2025-05-16 00:06:33 +0300

    overwritten poorly written food database and created user database to save data

--- FoodDataBase/AppDbContext.cs
+++ FoodDataBase/AppDbContext.cs
@@ -1,21 +1,60 @@
-﻿using FoodDataBase;
+﻿using FoodDataBase.Entities;
 using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
     public DbSet<Food> Foods { get; set; }
+    
+    public DbSet<FoodEntry> FoodEntries { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext()
+    {
+        this.Database.EnsureCreated();
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=localhost;Database=FoodDatabase;Trusted_Connection=True;");
+        optionsBuilder.UseSqlServer(@"
+                                         Data Source = FRASP\SQLEXPRESS;
+                                         Initial Catalog = Company_PD_421;
+                                         Integrated Security = True;
+                                         Connect Timeout = 2;
+                                         Encrypt = False;
+                                         Trust Server Certificate = True;
+                                         Application Intent = ReadWrite;
+                                         Multi Subnet Failover = False" );
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        
+        modelBuilder.Entity<FoodEntry>()
+            .HasOne(fe => fe.Food)
+            .WithMany()
+            .HasForeignKey(fe => fe.FoodId)
+            ;
+
+        modelBuilder.Entity<FoodEntry>()
+            .HasOne(fe => fe.User)
+            .WithMany(u => u.FoodEntries)
+            .HasForeignKey(fe => fe.UserId)
+            ;
+
+
+
+
+        // ініціалізуєм трохи значень (винесіть це в отдельний клас пж і зробіть набагатоооо побільше дефолтних значень)
+
         modelBuilder.Entity<Food>().HasData(
-            new Food { FoodId = 1, FoodName = "Apple", CaloriesPer100g = 52},
-            new Food { FoodId = 2, FoodName = "Banana", CaloriesPer100g = 89},
-            new Food { FoodId = 3, FoodName = "Tomato", CaloriesPer100g = 18}
+            new Food { Id = 1, Name = "Apple", CaloriesPer100g = 52 },
+            new Food { Id = 2, Name = "Banana", CaloriesPer100g = 96 }
         );
+
+        modelBuilder.Entity<User>().HasData(
+            new User { Id = 1, Gmail = "pavlosamchyk@gmail.com", PasswordHash = "Test1234" }
+            );
+
     }
 }
---
+++ FoodDataBase/Entities/FoodEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace FoodDataBase.Entities;
+
+public class FoodEntry
+{
+    public int Id { get; set; }
+    public int FoodId { get; set; }
+    public Food Food { get; set; }
+
+    public double Grams { get; set; }
+    public DateTime Date { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+}
