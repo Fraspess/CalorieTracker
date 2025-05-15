ba8303a3d350947ca4a0ee14f816c90f1ddeb098
Макар <stepanukmakar77@gmail.com>
2025-05-15 19:06:05 +0300

    Food database

--- FoodDataBase/Class1.cs
+++ FoodDataBase/Class1.cs
@@ -1,7 +1,17 @@
-﻿namespace FoodDataBase
-{
-    public class Class1
-    {
+﻿using System;
+using System.Linq;
 
+class Program
+{
+    static void Main()
+    {
+        using var db = new AppDbContext();
+        db.Database.EnsureCreated(); 
+
+        var allFoods = db.Foods.ToList();
+        foreach (var food in allFoods)
+        {
+            Console.WriteLine($"{food.FoodName} - {food.CaloriesPer100g} kcal");
+        }
     }
 }
---
+++ FoodDataBase/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDataBase
+{
+    public class Food
+    {
+        public int FoodId { get; set; }
+        public string? FoodName { get; set; }
+        public int CaloriesPer100g { get; set; }
+        public string? CodeLink { get; set; }
+    }
+}
